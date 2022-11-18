6aa1b0fd659b62e4cf09f9427834e78c2754e979
WillSantos02 <79523922+WillSantos02@users.noreply.github.com>
2022-11-18 00:48:33 -0300

    Commit da madrugada

--- js/interacao_card.js
+++ js/interacao_card.js
@@ -1,11 +1,14 @@
 const card_personalite = document.querySelector('.card_personalite');
 const card_creditos = document.querySelector('.card_creditos');
+const btn_creditos = document.querySelector('.btncreditos');
+
 // 
 card_personalite.addEventListener('click', () => {
     card_personalite.classList.contains('card_personalite') ? card_personalite.classList.replace('card_personalite', 'selecionado') : card_personalite.classList.replace('selecionado', 'card_personalite');
     if (card_creditos.classList.contains('selecionado_creditos'))
     {
-        card_creditos.classList.replace('selecionado_creditos', 'card_creditos')
+        card_creditos.classList.replace('selecionado_creditos', 'card_creditos');
+        btn_creditos.classList.replace('disable', 'btncreditos')
     }
 });
 
@@ -14,26 +17,6 @@ card_creditos.addEventListener('click', () => {
     if (card_personalite.classList.contains('selecionado'))
     {
         card_personalite.classList.replace('selecionado', 'card_personalite')
+        btn_creditos.classList.replace('btncreditos', 'disable')
     }
 });
-
-
-// document.querySelectorAll('.card_personalite').forEach(card => {
-//     card.addEventListener('click', event => {
-//         if(card.classList.contains('card_personalite')) {
-//             card.classList.replace('card_personalite', 'selecionado');
-//         } else {
-//             card.classList.replace('selecionado', 'card_personalite');
-//         }
-//     });
-// });
-
-// document.querySelectorAll('.card_creditos').forEach(card => {
-//     card.addEventListener('click', event => {
-//         if(card.classList.contains('card_creditos')){
-//             card.classList.replace('card_creditos', 'selecionado_creditos');
-//         } else {
-//             card.classList.replace('selecionado_creditos', 'card_creditos');
-//         }
-//     });
-// });
