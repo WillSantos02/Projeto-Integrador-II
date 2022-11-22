75fdcbb18f49d1d6fe41aab22bde0c395b0b612f
WillSantos02 <79523922+WillSantos02@users.noreply.github.com>
2022-11-22 02:13:06 -0300

    Ajustes na estrutura do código e nomeações de clas

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,15 @@
 function iniciaModal(){
     const modal = document.getElementById('termAcceptance');
-    modal.classList.add('mostrar');
+    modal.classList.add('show');
     
     modal.addEventListener('click', (e) => {
         console.log(e.target.id);
 
         if(e.target.id == 'termAcceptance' || e.target.id == 'btnGenerate' || e.target.id == "cancel"){
-            modal.classList.remove('mostrar');
+            modal.classList.remove('show');
         }
     });
 }
 
-const btnext = document.querySelector('.btnext');
-btnext.addEventListener('click', () => iniciaModal('termAcceptance'));
+const openBtn = document.querySelector('.generic-btn');
+openBtn.addEventListener('click', () => iniciaModal('termAcceptance'));
